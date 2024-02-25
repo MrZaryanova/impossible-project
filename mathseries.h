433cadec057a6e7101debbdec8d0a9e0227b581b
MrZaryanova <149221093+MrZaryanova@users.noreply.github.com>
2024-02-24 21:13:30 -0500

    added imprecise sine function

--- mathseries.h
+++ mathseries.h
@@ -1,1 +1,20 @@
+double sin(double x) {
+    if (x > -3.14 || x < 3.14) {
+    return x - (x * x * x)/6.5 + (x * x * x * x * x)/180;
+} else {
+    double z = x;
+    if (z > 0) { 
+        while (z > 3.14) {
+            z - 6.2831;
+        }
+    return z - (z * z * z)/6.5 + (z * z * z * z * z)/180;
+    } else {
 
+        while (-3.14 > z) {
+            z + 6.2831;
+        }
+    return z - (z * z * z)/6.5 + (z * z * z * z * z)/180;
+    }
+}
+
+}
