24ca90b2748ee76de9dbb7f98991204163518827
MrZaryanova <149221093+MrZaryanova@users.noreply.github.com>
2024-03-14 00:02:54 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,17 @@
+typedef struct {
+    char* test;
+    int x;
+} PROGRAM;
+
+PROGRAM* disc(const char *arg) {
+    return 0x0;
+}
+
+unsigned int disc2(char *arg1, unsigned int area, PROGRAM *input) {
+    const char length = 2;
+}
+
+
 int main() {
     char ac;
     char x;
@@ -5,4 +19,9 @@ int main() {
     char p;
     char sp;
     unsigned short pc;
+
+    PROGRAM* program; // do not free
+    program = disc("test");
+
+    return 0;
 }
