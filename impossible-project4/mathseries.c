59e5fc7a0f448d50b16ad4d242ab16735339b65f
MrZaryanova <siulmm26@gmail.com>
2026-04-16 14:16:59 -0400

    Refactor sin function for better range handling

--- impossible-project4/mathseries.c
+++ impossible-project4/mathseries.c
@@ -1,25 +1,28 @@
-double sin(double x) {
-    if (x > -3.14 && x < 3.14) {
-    return x - (x * x * x)/6.0 + (x * x * x * x * x)/180;
-} else {
-    double z = x;
-    if (z > 0) { 
-        while (z > 3.14) {
-           z = z - 6.2831;
-        }
-    return z - (z * z * z)/6.5 + (z * z * z * z * z)/180;
-    } else {
+#include "mathseries.h"
 
-        while (-3.14 > z) {
-            z = z + 6.2831;
-        }
-    return z - (z * z * z)/6.5 + (z * z * z * z * z)/180;
-    }
+double sin(double x) {
+    if (!(x > -PI && x < PI)) {
+        if(x > 0) {
+            return sin(x - (2 * PI));
+            } else {
+            return sin(x + (2 * PI));
+            }
 }
+    if(x < -1.5)
+        return 0.4 * x * x + 0.4 * PI * x;
+    if(x > 1.5)
+        return  -0.4 * x * x + 0.4 * PI * x;
+    
+    return x - (x * x * x)/6.0 + (x * x * x * x * x)/180;
 
 }
 
 double arcsin(double x) {
+    if(x < -1)
+        return -PI/2;
+    else if(x > 1)
+        return PI/2;
+        
     return (0.25 * x * x * x * x * x * x * x) + (0.075 * x * x * x * x * x) + (0.16666 * x * x * x) + x;
-    // remember that arcsin can only be used for values in range (-1, 1)
 }
+
