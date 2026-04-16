9fae04936f717fcf50c5d5f8d7d8873ce9697e8c
MrZaryanova <siulmm26@gmail.com>
2026-04-16 14:21:24 -0400

    Add header file for audio processing functions

---
+++ impossible-project4/apu.h
@@ -0,0 +1,14 @@
+//
+//  apu.h
+//  impossible-project4
+//
+//  Created by luis on 4/1/26.
+//
+
+#ifndef APU_H
+#define APU_H
+
+SDL_AppResult create_audio(void);
+SDL_AppResult play_audio(double freq);
+
+#endif
